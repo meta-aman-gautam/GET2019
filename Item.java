49883d501e273831674f8e9ec475c7da9630e84d
AMAN GAUTAM <aman.gautam@metacube.com>
2019-07-16 11:35:11 +0530

    changes done

--- Item.java
+++ Item.java
@@ -1,13 +1,5 @@
 public class Item {
 
-	
-	public int product_quantity = 0;
-	public String product_itemname;
-	public double product_price;
-
-	/*public class Item {
-
-	
 	public int product_quantity = 0;
 	public String product_itemname;
 	public double product_price;
@@ -20,66 +12,26 @@ public class Item {
 		product_price = price;
 		product_quantity = 0;
 	}
-
 	
-	/* getter/setter method
-	 * 
+	/*
+	 * getter/setter method
 	 */
-
-	public int getPquantity() {
+	public int getProduct_quantity() {
 		return product_quantity;
 	}
 
-
-	public void setPquantity(int pquantity) {
-		this.product_quantity = pquantity;
+	public void setProduct_quantity(int product_quantity) {
+		this.product_quantity = product_quantity;
 	}
 
-
-	public String getPitemname() {
+	public String getProduct_itemname() {
 		return product_itemname;
 	}
 
-
-	public double getPprice() {
+	public double getProduct_price() {
 		return product_price;
 	}
-	
-	
 
-}
-
-	 * constructor is created of class Item
-	 */
-	public Item(String itemname, double price) {
-		product_itemname = itemname;
-		product_price = price;
-		product_quantity = 0;
-	}
-
-	
-	/* getter/setter method
-	 * 
-	 */
-
-	public int getPquantity() {
-		return product_quantity;
-	}
-
-
-	public void setPquantity(int pquantity) {
-		this.product_quantity = pquantity;
-	}
-
-
-	public String getPitemname() {
-		return product_itemname;
-	}
-
-
-	public double getPprice() {
-		return product_price;
-	}
 	
 	
 
