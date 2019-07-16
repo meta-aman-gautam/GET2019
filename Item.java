59d4c11f97df81c906be4c611ac5ea4c4b8c145c
AMAN GAUTAM <aman.gautam@metacube.com>
2019-07-16 11:31:17 +0530

    changed done

--- Item.java
+++ Item.java
@@ -1,18 +1,24 @@
 public class Item {
 
 	
-	public int pquantity = 0;
-	public String pitemname;
-	public double pprice ;
+	public int product_quantity = 0;
+	public String product_itemname;
+	public double product_price;
 
+	/*public class Item {
+
+	
+	public int product_quantity = 0;
+	public String product_itemname;
+	public double product_price;
 
 	/*
 	 * constructor is created of class Item
 	 */
 	public Item(String itemname, double price) {
-		pitemname = itemname;
-		pprice = price;
-		pquantity = 0;
+		product_itemname = itemname;
+		product_price = price;
+		product_quantity = 0;
 	}
 
 	
@@ -21,22 +27,58 @@ public class Item {
 	 */
 
 	public int getPquantity() {
-		return pquantity;
+		return product_quantity;
 	}
 
 
 	public void setPquantity(int pquantity) {
-		this.pquantity = pquantity;
+		this.product_quantity = pquantity;
 	}
 
 
 	public String getPitemname() {
-		return pitemname;
+		return product_itemname;
 	}
 
 
 	public double getPprice() {
-		return pprice;
+		return product_price;
+	}
+	
+	
+
+}
+
+	 * constructor is created of class Item
+	 */
+	public Item(String itemname, double price) {
+		product_itemname = itemname;
+		product_price = price;
+		product_quantity = 0;
+	}
+
+	
+	/* getter/setter method
+	 * 
+	 */
+
+	public int getPquantity() {
+		return product_quantity;
+	}
+
+
+	public void setPquantity(int pquantity) {
+		this.product_quantity = pquantity;
+	}
+
+
+	public String getPitemname() {
+		return product_itemname;
+	}
+
+
+	public double getPprice() {
+		return product_price;
 	}
 	
 	
--- ShoppingCart.java
+++ ShoppingCart.java
@@ -2,268 +2,231 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ShoppingCart {
+
+	/*
+	 * Object of class Item(pencil)
+	 */
+	static Item pencil = new Item("pencil", 5.0);
+	static Item eraser = new Item("eraser", 3.5);
+	static Item sharpner = new Item("sharpner", 6.9);
+	static Item scale = new Item("scale", 25.0);
+	static Item notebook = new Item("notebook", 55.5);
+
 	
+	/*
+	 * ArrayLists are for the storing of values
+	 */
+	static ArrayList<Item> cart = new ArrayList<Item>();
+
 	
-	/* Object of class Item(pencil)
-	 * */
-	static Item pencil = new Item("pencil",5.0); 
-    static Item eraser = new Item("eraser",3.5);
-    static Item sharpner = new Item("sharpner",6.9);
-    static Item scale = new Item("scale" ,25.0);
-    static Item notebook = new Item("notebook" ,55.5);
-    
-    
-    /*
-     * ArrayLists are  for the storing of values 
-     */
-    
-    static ArrayList<Item> items = new ArrayList<Item>() ;
-    static ArrayList<Item> cart = new ArrayList<Item>() ;
-    
+	/**
+	 * add method is used to add item to the cart.
+	 * 
+	 * @param {string}item -item is the object of class Item
+	 * @param {integer}quantity
+	 */
+	public static void add(Item item, int quantity) {
+		if (cart.contains(item)) {
+			System.out.println("This Item already exists");
+
+		} else {
+
+			if (quantity > 0) {
+				cart.add(item);
+				item.product_quantity = quantity;
+				System.out.println("Item Added succesfully\n");
+			}
+		}
+	}
+
+	
+	/**
+	 * update method is used to update item to the cart.
+	 * 
+	 * @param {string}item -item is the object of class Item
+	 * @param {integer}quantity
+	 */
+	public static void update(Item item, int quantity) {
+
+		if (cart.contains(item)) {
+			/*
+			 * setting quantity as negative or zero will remove the product from
+			 * the cart . 
+			 */
+			if (quantity <= 0) {
+				cart.remove(item);
+				System.out.println("Item removed succesfully\n");
+			} else {
+				item.product_quantity = quantity;
+				System.out.println("Item updated succesfully\n");
+			}
+
+		}
+	}
+
+	
+	/**
+	 * showCart method is used to DISPLAY items of the cart.
+	 */
+	public static void showCart() {
+		
+		System.out.println("\nItem Name\t" + "Item Qty\t"+ "Item price(/piece)");
+		System.out.println("==========\t" + "========\t" + "==================");
+		for (Item value : cart) {
+			System.out.println(value.product_itemname + "   \t   "
+							 + value.product_quantity + "   \t   "
+							 + value.product_price);
+		}
+		System.out.println("==========\t" + "========\t" + "==================");
+	}
+
+	
+	/**
+	 * generateBills method is used to generate bill of cart,
+	 * along with the total price of all the items present in
+	 * the cart
+	 */
+	public static void generateBills() {
+		double total = 0;
+		
+		System.out.println("\nItem Name\t" + "Item Qty\t" + "Price(/piece)\t"
+				+ "Price");
+		System.out.println("=========\t" + "========\t" + "=============\t"
+				+ "=====");
+		for (Item value : cart) {
+			System.out.println(value.product_itemname + "   \t   "
+							+ value.product_quantity  + "   \t   "
+							+ value.product_price     + "   \t   "
+							+ (value.product_price * value.product_quantity));
+
+			/*
+			 * caluculating total price of the all the products .
+			 */
+			total = total + (value.product_price * value.product_quantity);
+		}
+
+		System.out.println("=========\t" + "========\t" + "=============\t"
+				+ "=====");
+
+		System.out.println("\nTOTAL = " + total + "\n");
+	}
+
+	
+	/**
+	 * Main method
+	 * 
+	 * @param {String} args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		int menu;
-		int option ;
-	    double price ;
-	    int quantity;
-	    String itemname ;
-	 
-	    
-	    Scanner sc = new Scanner(System.in) ;
-	    
-	
-	    
-	    System.out.println("Items Available (select specific options to choose from ) :-");
-	    System.out.println("[options]\t [ItemName]\t[Price(Rs)]");
-	    System.out.println("---------\t ----------\t ----------");
-	    System.out.println("1   \t\t  Pencil  \t  5.0");
-	    System.out.println("2   \t\t  Eraser  \t  3.5");
-	    System.out.println("3   \t\t  Sharpner  \t  6.9");
-	    System.out.println("4   \t\t  Scale   \t  25.0");
-	    System.out.println("5   \t\t  Notebook  \t  55.5\n");
-		
-	    
-	
-		
-	    do{
-	    	System.out.println("[To add item press '1']\n[To update cart press '2']\n[To Show Cart press '3']\n[To generate bill press '4']");
-		    menu = sc.nextInt();
-			if(menu == 1)
-			{
-				System.out.print("Choose from Available Option to Add items");	
+
+		int menu, option, quantity;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out
+				.println("Items Available (select specific options to choose from ) :-");
+		System.out.println("[options]\t [ItemName]\t[Price(Rs)]");
+		System.out.println("---------\t ----------\t ----------");
+		System.out.println("1   \t\t  Pencil  \t  5.0");
+		System.out.println("2   \t\t  Eraser  \t  3.5");
+		System.out.println("3   \t\t  Sharpner  \t  6.9");
+		System.out.println("4   \t\t  Scale   \t  25.0");
+		System.out.println("5   \t\t  Notebook  \t  55.5\n");
+
+		do {
+			System.out.println("[To add item press '1']\n"
+							+ "[To update cart press '2']\n"
+							+ "[To Show Cart press '3']\n"
+							+ "[To generate bill press '4']");
+			
+			menu = sc.nextInt();
+			
+			if (menu == 1) {
+				System.out.print("Choose from Available Option to Add items");
 				option = sc.nextInt();
 				System.out.print("Enter the quantity");
 				quantity = sc.nextInt();
-				
-				switch(option){
-				
+
+				switch (option) {
+
 				case 1:
-				    {
-					  add(pencil , quantity);
-					  break;
-					}
+					add(pencil, quantity);
+					break;
+
 				case 2:
-					{
-					  add(eraser , quantity);
-					  break;
-					}
+					add(eraser, quantity);
+					break;
+
 				case 3:
-					{
-					  add(sharpner , quantity);
-					  break;
-					}
+					add(sharpner, quantity);
+					break;
+
 				case 4:
-					{
-					  add(scale , quantity);
-					  break ;
-					}
+					add(scale, quantity);
+					break;
+
 				case 5:
-				    {
-					  add(notebook, quantity);
-					  break ;
-					}
-				default :
+					add(notebook, quantity);
+					break;
+
+				default:
 					System.out.println(" XXX Invalid Choice ...pls try again XXX ");
-					
+
 				}
-				
+
 			}
-			
-			
-			
-			else if(menu == 2)
-			{
-			    System.out.print("Choose from Available Option to update/remove items");	
+
+			else if (menu == 2) {
+				System.out
+						.print("Choose from Available Option to update/remove items");
 				option = sc.nextInt();
-				System.out.println("Enter the quantity[ quantity as negative or zero will remove the product from the cart]");
-				quantity =sc.nextInt();
-				switch(option){
-				
+				System.out
+						.println("Enter the quantity[ quantity as negative or zero will remove the product from the cart]");
+				quantity = sc.nextInt();
+				switch (option) {
+
 				case 1:
-				    {
-					  if(cart.contains(pencil))
-					  {
-						// setting quantity as negative or zero will remove the product from the cart
-						if(quantity<=0)
-						{
-							cart.remove(pencil);
-							 System.out.println("*** Item removed succesfully***\n");
-						}
-						else
-						{
-						  pencil.pquantity = quantity;
-						  System.out.println("*** Item updated succesfully ***\n");
-						}
-						  
-					  }
-						  
-					  break;
-					}
+					update(pencil, quantity);
+					break;
+
 				case 2:
-					{
-						if(cart.contains(eraser))
-						  {
-							if(quantity<=0)
-							{
-								cart.remove(eraser);
-								 System.out.println("*** Item removed succesfully***\n");
-							}
-							else
-							{
-							  eraser.pquantity = quantity;
-							  System.out.println("*** Item updated succesfully***\n");
-							}
-							  
-						  }
-					  break;
-					}
+					update(eraser, quantity);
+					break;
+
 				case 3:
-					{
-						if(cart.contains(sharpner))
-						  {
-							if(quantity<=0)
-							{
-								cart.remove(sharpner);
-								System.out.println("*** Item removed succesfully ***\n");
-							}
-							else
-							{
-							  sharpner.pquantity = quantity;
-							  System.out.println("*** Item updated succesfully ***\n");
-							}
-							  
-						  }
-					  break;
-					}
+					update(sharpner, quantity);
+					break;
+
 				case 4:
-					{
-						if(cart.contains(scale))
-						  {
-							if(quantity<=0)
-							{
-								cart.remove(scale);
-								System.out.println("*** Item removed succesfully ***\n");
-							}
-							else
-							{
-							  scale.pquantity = quantity;
-							  System.out.println("*** Item updated succesfully ***\n");
-							}
-							  
-						  }
-					}
+					update(scale, quantity);
+					break;
+
 				case 5:
-				    {
-				    	if(cart.contains(notebook))
-						  {
-							if(quantity<=0)
-							{
-								cart.remove(notebook);
-								System.out.println("*** Item removed succesfully ***\n");
-								
-							}
-							else
-							{
-							  notebook.pquantity = quantity;
-							  System.out.println("*** Item updated succesfully ***\n");
-							}
-							  
-						  }
-					}
-				
+					update(notebook, quantity);
+					break;
+				default:
+					System.out.println(" XXX Invalid Choice ...pls try again XXX ");
+
 				}
 			}
-			
-			
-			else if(menu == 3)
-			{
+
+			else if (menu == 3) {
 				showCart();
 			}
-			
-			else if(menu == 4)
-            {
-				
+
+			else if (menu == 4) {
+
 				generateBills();
 			}
-			
-			else 
-            {
-				
-			System.out.println("Please choose from the available options\n");
-		    continue ;
-			}
-			
-			
-			
-		
-		}while(menu!= 0);
-		
-		}
-	
 
-	    
-	  public static void add(Item item, int quantity)
-	  {
-		  if(cart.contains(item))
-		  {
-			  System.out.println("=> This Item already exists <=");
-			  
-		  }
-		  else
-		  {
-		       
-		      if(quantity>0)
-		      {
-		      cart.add(item);
-		      item.pquantity = quantity;
-		      System.out.println("*** Item Added succesfully\n");
-		      }
-		  }
-	  }
-	    
-	 
-	    
-	  public static void showCart()
-	  {
-	    	System.out.println("\nItem Name\t Item Qty\t Item price(/piece)");
-	    	System.out.println("=========\t ========\t ==================");
-	    	for (Item value : cart)
-	    	{
-	    	System.out.println(value.pitemname+"   \t   "+value.pquantity+"   \t\t\t   "+value.pprice);
-	    	}
-	  }
-	    
-	  public static void generateBills()
-	  {   double total = 0 ;
-		  System.out.println("\nItem Name\t Item Qty\t Item price(/piece)\t Price");
-		  System.out.println("=========\t ========\t ==================\t=======");
-		  for(Item value : cart)
-		  {
-	      System.out.println(value.pitemname+"   \t   "+value.pquantity+"   \t\t\t   "+value.pprice+"   \t   "+(value.pprice*value.pquantity));  
-		  total = total+(value.pprice*value.pquantity);
-		  }
-		  System.out.println("=========\t ========\t ==================\t=======");
-		  System.out.println("\nTOTAL = "+total+"\n");
-		  }
+			else {
+
+				System.out.println("Please choose from the available options\n");
+				continue;
+			}
+
+		} while (menu != 0);
+
+	}
+
 }
