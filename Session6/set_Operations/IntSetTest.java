8e69ccfeafa8b0ceced687ee4999a0eb725cf65a
meta-aman-gautam <52689565+meta-aman-gautam@users.noreply.github.com>
2019-07-23 00:02:19 +0530

    Update IntSetTest.java

--- Session6/set_Operations/IntSetTest.java
+++ Session6/set_Operations/IntSetTest.java
@@ -13,7 +13,6 @@ import org.junit.runners.Parameterized;
 @RunWith(Parameterized.class)
 public class IntSetTest {
 	
-
 	private int member  ;
 	private boolean value ;
 	private boolean expectedValue ;
@@ -24,8 +23,6 @@ public class IntSetTest {
 	private int Array[];
 	private int []intSet_Expected;
 	private int intSet_1[];
-	
-	
 	
 	public IntSetTest(boolean expectedValue ,boolean intSet , 
 			int expectedLength ,int sizeOfSet,
@@ -44,6 +41,7 @@ public class IntSetTest {
 		//is Subset Test
 		this.value = value ;
 		this.subSet = subSet ;
+		
 		//compliment 
 		this.Array =Array ;
 		
@@ -52,24 +50,21 @@ public class IntSetTest {
 		this.intSet_1 = intSet_1 ;
 	}
 	
-	
-	
 	@Parameterized.Parameters
 	public static Collection data() {
 		return Arrays.asList(new Object[][] {
-				{   true ,new IntSet(new int[]{1, 3, 5, 8, 12, 15, 18, 40 }).isMember(5),
+				{   	true ,new IntSet(new int[]{1, 3, 5, 8, 12, 15, 18, 40 }).isMember(5),
 					new IntSet(new int[]{1, 3, 5, 8, 12, 15, 18, 40 }).set.length ,new IntSet(new int[]{1, 3, 5, 8, 12, 15, 18, 40 }).sizeOfSet(),
 					new IntSet(new int[] { 1, 3, 5, 8, 12, 15, 18,40 }).isSubset(new IntSet(new int[] { 8, 15, 40 })),true,
 					new IntSet(new int[] { 1, 3, 5, 8, 12, 15, 18, 40 }).getCompliment().set,
 					new int[] { 1, 3, 5, 8, 12, 15, 18, 40, 2, 4, 7 } , IntSet.union(new IntSet(new int[] { 1, 3, 5, 8, 12, 15, 18, 40 }), new IntSet(new int[] { 3, 2, 5, 4, 7, 8 })).set },
 				
-				{	new IntSet(new int[]{1, 3, 5, 8, 12, 15, 18, 40 }).isMember(18),true ,
+				{	true ,new IntSet(new int[]{1, 3, 5, 8, 12, 15, 18, 40 }).isMember(18),
 					new IntSet(new int[]{1, 3, 5, 8, 12, 15, 18, 40 }).set.length ,new IntSet(new int[]{1, 3, 5, 8, 12, 15, 18, 40 }).sizeOfSet(),
 					new IntSet(new int[] { 1, 3, 5, 8, 12, 15, 18,40 }).isSubset(new IntSet(new int[] { 8, 15, 40 })),true,
 					new IntSet(new int[] { 1, 3, 5, 8, 12, 15, 18, 40 }).getCompliment().set,
 					new int[] { 1, 3, 5, 8,  40, 2, 4, 7 } , IntSet.union(new IntSet(new int[] { 1, 3, 5, 8, 40 }), new IntSet(new int[] { 3, 2, 5, 4, 7, 8 })).set },
-					
-		
+						
 		});
 	}
 	
@@ -77,44 +72,45 @@ public class IntSetTest {
 	 * Test case when input set is empty
 	 */
 	@Test(expected = AssertionError.class)
-	public void setIsEmptyTest() {
+	public void testForEmptySet() {
 		IntSet intSet = new IntSet(new int[] {});
 	}
 	
-	@Test
-	public void isMemberTest() {
+	@Test  //test wether value is a member of set or not
+	public void testForMembership() {
 		assertEquals(expectedValue, intSet);
 	}
 
-	@Test
-	public void sizeTest() {
-		//IntSet intSet = new IntSet(new int[] { 1, 3, 5, 8, 12, 15, 18, 40 });
+	@Test  //test for Size of Set 
+	public void testForSizeOfSet() {
 		assertEquals(expectedLength, sizeOfSet);
 	}
 
-	@Test
-	public void isSubSetTest() {
+	@Test   //test for Subset
+	public void testForSubset() {
 		assertEquals(value, subSet);
 	}
 
-	@Test
-	public void complimentTest() {
+	@Test  //test for compliment
+	public void testForCompliment() {
+		/*
+		*here the intSet is taken before hand 
+		*/
 		IntSet intSet = new IntSet(new int[] { 1, 3, 5, 8, 12, 15, 18, 40 });
 		int[] expectedArray = new int[1000 - intSet.set.length];
-		int j = 0;
-		for (int i = 0; i < 1000; i++) {
-			if (!(intSet.isMember(i + 1))) {
-				expectedArray[j] = i + 1;
-				j++;
+		int count_j = 0;
+		for (int count_i= 0;count_i < 1000; count_i++) {
+			if (!(intSet.isMember(count_i + 1))) {
+				expectedArray[count_j] = count_i + 1;
+				count_j++;
 			}
 		}
 		assertArrayEquals(expectedArray, Array);
 	}
 	
-	@Test
-	public void unionTest() {
+	@Test  //union TestCase
+	public void testForSetUnion() {
 		assertArrayEquals(intSet_Expected,intSet_1);
 	}
-	
 
 }
