2eddff6e3aa7ee59fd063c63cfb16155d8003720
unknown <meta-aman-gautam>
2019-08-01 01:00:26 +0530

    DSA/Session_1

---
+++ DSA_Session1/linkedlistOperation/Driver.java
@@ -0,0 +1,31 @@
+package linkedlistOperation ;
+/**
+ * 
+ * @author Aman
+ *
+ */
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		MyLinkedList list = new MyLinkedList();
+
+		list.addToList(list, 1);
+		list.addToList(list, 2);
+		list.addToList(list, 3);
+		list.addToList(list, 4);
+		list.addToList(list, 5);
+		list.addToList(list, 6);
+		list.addToList(list, 7);
+		
+		list.display(list);
+		
+		list.rotateSubString(list, 2, 3,1);
+		list.display(list);
+		System.out.println("____________\n");
+	
+		list.createLoop(1);
+		System.out.println(list.checkLoop());
+	}
+
+}
