6967bfc12f860f0e159bd365a14d16d0bd7aaa9f
Aman Gautam <aman.gautam@metacube.com>
2019-10-21 17:51:42 +0530

    UI_Session_10

---
+++ UI-Session-10/appDir/task-management-application/src/app/task-label/task-label.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-label',
+  templateUrl: './task-label.component.html',
+  styleUrls: ['./task-label.component.css']
+})
+export class TaskLabelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
